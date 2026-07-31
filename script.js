89b2f6a9ff058bde14d197f42497ed7ab71443da
thekeshavraj01-del <thekeshavraj01@gmail.com>
2026-07-31 16:11:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("button");
+
+  button.addEventListener("click", () => {
+    alert(
+`❤️ Hey Babeee ❤️
+
+This is only the beginning...
+
+Every page of this website is made with love.
+
+— From Your Azeeb Insan ❤️`
+    );
+  });
+});
